33710fab503edef5471c16f94abfa39e97de07e2
Alan Gorton <dev.code1up@googlemail.com>
2012-07-30 13:42:02 +0200

    Update signin.sh

--- signin.sh
+++
@@ -1,3 +0,0 @@
-# curl -d "username=alanjgorton@googlemail.com&password=p44sword" http://localhost:3000/api/signin
-curl -d "username=bob@example.com&password=password" http://localhost:3000/api/signin
-
